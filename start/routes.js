5d1080de7441db9c1910fa306c1ab5739c3781e5
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-05-09 12:25:55 -0300

    Create route for cities

--- start/routes.js
+++ start/routes.js
@@ -21,4 +21,5 @@ Route.post('/authenticate', 'AuthController.authenticate')
 
 Route.get('/app',  'AppController.index').middleware(['auth'])
 
-Route.resource('campings', 'CampingController').apiOnly().middleware(['auth'])
+Route.resource('campings', 'CampingController').apiOnly()
+Route.resource('cities', 'CityController').apiOnly()
