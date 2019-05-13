372df63fc2a5957a15ecd4ea035c67a8c3170a10
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-05-13 10:31:12 -0300

    Adding hashids

--- app/Models/City.js
+++ app/Models/City.js
@@ -2,8 +2,16 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+const Hashids = use('Hashids')
 
 class City extends Model {
+
+  static get computed () {
+    return ['hashids']
+  }
+  getHashids({ id }) {
+    return Hashids.encode(id)
+  }
 
   campings() {
     return this.hasMany('App/Models/Camping')
