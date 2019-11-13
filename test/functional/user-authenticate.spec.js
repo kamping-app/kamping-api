0fbd5d5e79c2a24dee853310f072addc5c079ed5
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-11-12 21:54:19 -0300

    Adding test for session

---
+++ test/functional/user-authenticate.spec.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const { test, trait } = use('Test/Suite')('User Authenticate')
+
+trait("Test/ApiClient");
+trait("Auth/Client");
+trait("DatabaseTransactions");
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const User = use("App/Models/User");
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use("Factory");
+
+test('it should return JWT when session created', async ({ assert, client }) => {
+
+  const sessionPayload = {
+    email: 'vicainelli@gmail.com',
+    password: '123456'
+  }
+
+  await Factory
+    .model('App/Models/User')
+    .create(sessionPayload)
+
+  const response = await client
+    .post('/authenticate')
+    .send(sessionPayload)
+    .end()
+
+    response.assertStatus(200)
+    assert.exists(response.body.token)
+})
